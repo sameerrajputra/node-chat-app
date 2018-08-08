251e06d72ac6673d53910ec5ad043c4e4cca45b9
sameerrajputra <sameer.rajputra55@gmail.com>
2018-08-08 19:14:06 +0545

    Added styling and jquery features with an error

--- public/js/index.js
+++ public/js/index.js
@@ -51,12 +51,13 @@ socket.on('newLocationMessage', function(message) {
 
 jQuery('#message-form').on('submit', function(e){
 	e.preventDefault();
+	var messageTextBox = jQuery('[name=message]');
 
 	socket.emit('createMessage', {
 		from: 'User',
-		text: jQuery('[name=message]').val()
+		text: messageTextBox.val()
 	}, function(){
-
+		jQuery('[name=message]').val();
 });
 });
 
@@ -66,12 +67,16 @@ locationButton.on('click', function(){
 		return alert('Your browser is not compatible with the geolocation API');
 	}
 
+	locationButton.attr('disabled', 'disabled').text('Sending location...');
+
 	navigator.geolocation.getCurrentPosition(function(position){
+		locationButton.removeAttr('disabled').text('Send Location');
 		socket.emit('createLocationMessage', {
 			latitude: position.coords.latitude,
 			longitude: position.coords.longitude
 		});
 	}, function(){
+		locationButton.removeAttr('disabled').text('Send Location');
 		alert('Unable to fetch location');
 	});
 });
--- server/server.js
+++ server/server.js
@@ -3,69 +3,22 @@ const http = require('http');
 const express = require('express');
 const socketIO = require('socket.io');
 
-const {generateMessage, generateLocationMessage} = require('./utils/message');
-const publicPath = path.join(__dirname , '/../public');
+const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 var app = express();
 var server = http.createServer(app);
 var io = socketIO(server);
 
-
 app.use(express.static(publicPath));
 
 io.on('connection', (socket) => {
-	console.log('New User connected');
+  console.log('New user connected');
 
-// 	socket.emit('newEmail',{
-// 	from:'Ram',
-// 	text: 'Hey you!',
-// 	createdAt:'123'
-// });
-
-// 	socket.on('createEmail', function(email){
-// 		console.log('Email created', email);
-// 	})
-
-	// socket.emit('newMessage', {
-	// 	from: 'sameer@example.com',
-	// 	text: 'Jenny are you all right?',
-	// 	createdAt: '123'
-	// });
-
-socket.emit('newMessage', generateMessage('Admin', 'Welcome to our chat app.'));
-
-socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user Joined'));
-
-	socket.on('createMessage', function(message){
-		console.log('created Message ', message);
-
-		io.emit('newMessage', generateMessage(message.from, message.text));
+  socket.on('disconnect', () => {
+    console.log('User was disconnected');
+  });
 });
-/////////////////////This is for broadcasting message to every other socket except for this sending socket
-	// 	socket.broadcast.emit('newMessage', {
-	// 		from: message.from,
-	// 		text: message.text,
-	// 		createdAt: new Date().getTime()
-	// 	});
-	// });
-
-	socket.on('testMessage', (message, callback) => {
-		console.log('TEst message', message);
-		io.emit('newMessage', generateMessage(message.from, message.text));
-		callback('What the hell is this?');
-	});
-
-	socket.on('createLocationMessage', (location) => {
-		io.emit('newLocationMessage', generateLocationMessage('Admin', location.latitude, location.longitude));
-	});
-
-	socket.on('disconnect', () => {
-		console.log('New user disconnected');
-	})
-});
-	
-
 
 server.listen(port, () => {
-	console.log(`App is running on port: ${port}`);
+  console.log(`Server is up on ${port}`);
 });
